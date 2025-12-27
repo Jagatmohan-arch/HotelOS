ab72bc939933448d4ae2af70d1f495fa3501608b
Jagatmohan-arch <jmdotaryan@gmail.com>
2025-12-27 13:55:11 +0530

    Fix: Add CSP meta tag to app.php layout to allow CDN scripts

--- views/layouts/app.php
+++ views/layouts/app.php
@@ -34,6 +34,14 @@ $breadcrumbs = $breadcrumbs ?? [];
     
     <!-- Security -->
     <meta name="csrf-token" content="<?= htmlspecialchars($csrfToken ?? '') ?>">
+    <meta http-equiv="Content-Security-Policy" content="
+        default-src 'self';
+        script-src 'self' 'unsafe-inline' 'unsafe-eval' https://unpkg.com https://cdn.jsdelivr.net https://cdn.tailwindcss.com;
+        style-src 'self' 'unsafe-inline' https://fonts.googleapis.com https://cdn.jsdelivr.net;
+        font-src 'self' https://fonts.gstatic.com;
+        img-src 'self' data: https:;
+        connect-src 'self';
+    ">
     
     <!-- Preconnect -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
